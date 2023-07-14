64a682f273c762f8c3e1aae8fea48a0e85ae3196
zvek6 <zvi@beck.co.il>
2023-07-14 13:47:55 +0300

    Added background

--- code/app-ui/src/App.js
+++ code/app-ui/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, FormControl, FormLabel, Grid, Switch, TextField } from '@mui/material';
+import './App.css';
 
 const App = () => {
   const [file, setFile] = useState('');
@@ -26,64 +27,76 @@ const App = () => {
   const language = isEnglish ? 'English' : 'עברית';
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Button variant="contained" onClick={handleLanguageChange}>
-          {language}
-        </Button>
+    <div className="app-container">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Button variant="contained" onClick={handleLanguageChange}>
+            {language}
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <FormLabel>{isEnglish ? 'Choose a file:' : 'בחר קובץ:'}</FormLabel>
+            <input type="file" onChange={handleFileChange} />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel>
+              {isEnglish
+                ? 'Enter number of pages in each booklet (In multiples of 4. the standard is 32):'
+                : 'הכנס מספר העמודים בכל מחברת (בכפולות של 4, הסטנדרט הוא 32):'}
+            </FormLabel>
+            <TextField
+              type="text"
+              size="small"
+              value={numberOfPages}
+              onChange={(e) => setNumberOfPages(e.target.value)}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <FormControl fullWidth>
+            <FormLabel>
+              {isEnglish ? 'Enter pages per sheet (2/4/8/16):' : 'הכנס מספר עמודים לעמוד (2/4/8/16):'}
+            </FormLabel>
+            <TextField
+              type="text"
+              size="small"
+              value={pagesPerSheet}
+              onChange={(e) => setPagesPerSheet(e.target.value)}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl component="fieldset">
+            <FormLabel>
+              {isEnglish ? 'Sewing or gluing? In the gluing there is an extra blank page on each side.' : 'מודבק או תפור? בגרסא המודבקת יש תוספת של עמוד ריק בכל מחברת:'}
+            </FormLabel>
+            <Switch checked={isGluing} onChange={() => setIsGluing((prevState) => !prevState)} />
+            <span>
+              {isGluing ? (isEnglish ? 'Gluing' : 'מודבק') : (isEnglish ? 'Sewing' : 'תפור')}
+            </span>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl component="fieldset">
+            <FormLabel>
+              {isEnglish ? 'Only one notebook?' : 'האם כבודו מדפיס רק מחברת אחת?'}
+            </FormLabel>
+            <Switch checked={isOneNotebook} onChange={() => setIsOneNotebook((prevState) => !prevState)} />
+            <span>
+              {isOneNotebook ? (isEnglish ? 'Yes' : 'כן') : (isEnglish ? 'No' : 'לא')}
+            </span>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" onClick={handleSubmit}>
+            {isEnglish ? 'Do it' : 'יאללה לעבודה'}
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <FormControl fullWidth>
-          <FormLabel>{isEnglish ? 'Choose a file:' : 'בחר קובץ:'}</FormLabel>
-          <input type="file" onChange={handleFileChange} />
-        </FormControl>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <FormControl fullWidth>
-          <FormLabel>
-            {isEnglish
-              ? 'Enter number of pages in each booklet (In multiples of 4. the standard is 32):'
-              : 'הכנס מספר העמודים בכל מחברת (בכפולות של 4, הסטנדרט הוא 32):'}
-          </FormLabel>
-          <TextField type="text" size="small" value={numberOfPages} onChange={(e) => setNumberOfPages(e.target.value)} />
-        </FormControl>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <FormControl fullWidth>
-          <FormLabel>
-            {isEnglish ? 'Enter pages per sheet (2/4/8/16):' : 'הכנס מספר עמודים לעמוד (2/4/8/16):'}
-          </FormLabel>
-          <TextField type="text" size="small" value={pagesPerSheet} onChange={(e) => setPagesPerSheet(e.target.value)} />
-        </FormControl>
-      </Grid>
-      <Grid item xs={12}>
-        <FormControl component="fieldset">
-          <FormLabel>
-            {isEnglish ? 'Sewing or gluing? In the gluing there is an extra blank page on each side.' : 'מודבק או תפור? בגרסא המודבקת יש תוספת של עמוד ריק בכל מחברת:'}
-          </FormLabel>
-          <Switch checked={isGluing} onChange={() => setIsGluing((prevState) => !prevState)} />
-          <span>
-            {isGluing ? (isEnglish ? 'Gluing' : 'מודבק') : (isEnglish ? 'Sewing' : 'תפור')}
-          </span>
-        </FormControl>
-      </Grid>
-      <Grid item xs={12}>
-        <FormControl component="fieldset">
-          <FormLabel>
-            {isEnglish ? 'Only one notebook?' : 'האם כבודו מדפיס רק מחברת אחת?'}
-          </FormLabel>
-          <Switch checked={isOneNotebook} onChange={() => setIsOneNotebook((prevState) => !prevState)} />
-          <span>
-            {isOneNotebook ? (isEnglish ? 'Yes' : 'כן') : (isEnglish ? 'No' : 'לא')}
-          </span>
-        </FormControl>
-      </Grid>
-      <Grid item xs={12}>
-        <Button variant="contained" onClick={handleSubmit}>
-          {isEnglish ? 'Do it' : 'יאללה לעבודה'}
-        </Button>
-      </Grid>
-    </Grid>
+    </div>
   );
 };
 
