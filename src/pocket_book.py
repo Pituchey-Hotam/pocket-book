169282c1f7e79fc3693d9da868214d17dc0e7c6f
neria naiman <72870471+nnaiman@users.noreply.github.com>
2023-08-25 17:36:25 +0300

    System tests (#8)
    * basic tests
    
    * basic tests
    
    * basic tests
    
    * basic tests and cut lines
    
    * basic tests, cut lines and page numbers
    
    * basic tests, cut lines and page numbers
    
    * basic tests, cut lines and page numbers
    
    * gitignore have now some more important stuff
    
    * Moved the test and changed to pytest
    
    * Made the tests better
    
    * Added enum for page size
    
    * Even nicer test
    
    * Refactored more
    
    ---------
    
    Co-authored-by: zvek6 <zvi@beck.co.il>

--- src/pocket_book.py
+++ src/pocket_book.py
@@ -1,18 +1,31 @@
+import io
+import os
 from os import mkdir
 from os.path import exists
 from shutil import rmtree
-from math import ceil
+from math import ceil, log, sqrt
 from threading import Thread
+from enum import Enum
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from PySimpleGUI import theme, Button, Text, Input, InputText, FilesBrowse, Window, WIN_CLOSED, Image as guiImage
 
-import pdfbooklet_new
-from ui_settings import *
+import src.pdfbooklet_new as pdfbooklet_new
+from reportlab.lib.pagesizes import *
+from reportlab.pdfgen import canvas
+from src.ui_settings import *
 
-# Version - important
-currentVersion = "2.1.2"
 
+class PageSize(Enum):
+    """
+    An enum class for page size representation.
+    The value of the page size is how many pages in this size
+    fit in an A4 page.
+    """
+
+    A5 = 2
+    A6 = 4
+    A7 = 8
 
 # -----------------------
 #     pdf manipulation
@@ -36,7 +49,7 @@ def split(path, name_of_split, sp, length, bind_method='s'):
         if page < pdf.getNumPages():
             pdf_writer.addPage(pdf.getPage(page))
         else:
-            pdf_writer.addBlankPage()
+            addBP(pdf_writer, page)
     if not bind_method == 's':
         pdf_writer.insertBlankPage(0)
         pdf_writer.addBlankPage()
@@ -46,6 +59,26 @@ def split(path, name_of_split, sp, length, bind_method='s'):
     output_pdf.close()
 
 
+def addBP(pdfFileWriter, i):
+    """The function add blank page with number of page in notebook
+    :param i: number of page
+    """
+    packet = io.BytesIO()
+    can = canvas.Canvas(packet, pagesize=A4)
+    can.setFontSize(20)
+    can.drawString(A4[0] / 2, 10, str(i))
+    can.save()
+    packet.seek(0)
+    # create a new PDF with Reportlab
+    new_pdf = PdfFileReader(packet)
+    # read your existing PDF
+    existing_pdf = PdfFileWriter()
+    existing_pdf.addBlankPage(A4[0], A4[1])
+    # add the "watermark" (which is the new pdf) on the existing page
+    existing_pdf.pages[0].merge_page(new_pdf.pages[0])
+    pdfFileWriter.addPage(existing_pdf.pages[0])
+
+
 def split_Even_Odd(path, name_of_split):
     pdf = PdfFileReader(path)
     output_ev = f'{name_of_split}_even.pdf'
@@ -53,7 +86,7 @@ def split_Even_Odd(path, name_of_split):
     pdf_writer_ev = PdfFileWriter()
     pdf_writer_odd = PdfFileWriter()
     number_of_pages = extract_num_of_pages(path)
-    number_of_pages_plusblank = 0 # int((4 - (number_of_pages / 2 % 4)) * (number_of_pages / 2 % 4 > 0))
+    number_of_pages_plusblank = 0  # int((4 - (number_of_pages / 2 % 4)) * (number_of_pages / 2 % 4 > 0))
     for page in range(number_of_pages + number_of_pages_plusblank):
         if page < number_of_pages:
             if page % 2 == 0:
@@ -136,7 +169,7 @@ def combineMethod(trash_file, num):
 
 def moreThan(trash_file, combine_method, eng, num=1):
     n = str(num)
-    pev = str(num-1)
+    pev = str(num - 1)
     if pev == '0':
         pev = ''
 
@@ -160,7 +193,56 @@ def moreThan(trash_file, combine_method, eng, num=1):
     return odd_path, even_path
 
 
-def making_the_pdf(inputs, eng=0):
+"""
+def add_page_number(file_name):
+    tmp = open(file_name, 'rb')
+    pdfFileReader = PdfFileReader(tmp)
+    pdfFileWriter = PdfFileWriter()
+    for i in range(len(pdfFileReader.pages)):
+        packet = io.BytesIO()
+        can = canvas.Canvas(packet,
+                            pagesize=(pdfFileReader.pages[i].mediaBox.width, pdfFileReader.pages[i].mediaBox.height))
+        can.setFontSize(10)
+        can.drawString(pdfFileReader.pages[i].mediaBox.width / 2, 0, str(i + 1))
+        can.save()
+        packet.seek(0)
+        # create a new PDF with Reportlab
+        new_pdf = PdfFileReader(packet)
+        # read your existing PDF
+        existing_pdf = PdfFileWriter()
+        existing_pdf.addPage(pdfFileReader.pages[i])
+        # add the "watermark" (which is the new pdf) on the existing page
+        existing_pdf.pages[0].merge_page(new_pdf.pages[0])
+        pdfFileWriter.addPage(existing_pdf.pages[0])
+    tmp.close()
+    output = open(file_name + "2.pdf", "wb")
+    pdfFileWriter.write(output)"""
+
+
+def add_page_numbers(input_pdf, output_pdf):
+    pdf_reader = PdfFileReader(input_pdf)
+    pdf_writer = PdfFileWriter()
+
+    for page_number, page in enumerate(pdf_reader.pages, start=1):
+        packet = io.BytesIO()
+        c = canvas.Canvas(packet, pagesize=A4)
+        c.setFontSize(20)
+        page_number_text = f"{page_number}"
+        c.drawString(page.mediaBox.width / 2, 10, page_number_text)
+        c.save()
+
+        packet.seek(0)
+        new_pdf = PdfFileReader(packet)
+        page.mergePage(new_pdf.pages[0])
+        pdf_writer.addPage(page)
+
+    with open(output_pdf, "wb") as output_file:
+        pdf_writer.write(output_file)
+
+
+def making_the_pdf(inputs, eng=0, pNumber=False, cutLines=True):
+    if pNumber:
+        add_page_numbers(inputs[0], inputs[0])
     inputs[0] = inputs[0].split(';')
     for inp in inputs[0]:
         inp = inp.replace('\\', '/')
@@ -169,7 +251,7 @@ def making_the_pdf(inputs, eng=0):
         old_path = inp[:-len(file_name) - 1] + '/'
 
         dir_path = old_path + 'trash ' + file_name[:-4]
-        path = dir_path[:] + '\\'  # argv[2]+'\\'
+        path = dir_path[:] + '/'  # argv[2]+'\\'
         if not exists(path):
             mkdir(path)
 
@@ -201,9 +283,9 @@ def making_the_pdf(inputs, eng=0):
             split_Even_Odd(final_path, trash_file)
 
             counter = 1
-            while pages_per_sheet/(counter**2) > 1:
-                print(pages_per_sheet/(counter**2))
-                print(counter)
+            while pages_per_sheet / (counter ** 2) > 1:
+                # print(pages_per_sheet / (counter ** 2))
+                # print(counter)
                 odd_path, even_path = moreThan(trash_file, combine_method, eng, counter)
                 counter += 1
 
@@ -211,18 +293,65 @@ def making_the_pdf(inputs, eng=0):
             merge_sort_pdfs(odd_path, even_path, final_path)
 
         rmtree(dir_path, ignore_errors=False)
+    if cutLines:
+        add_dashed_cut_line(final_path, pages_per_sheet)
+
+
+def add_dashed_cut_line(file, numP):
+    pdf = PdfFileReader(file)
+    output_pdf = PdfFileWriter()
+    for i in range(len(pdf.pages)):
+        packet = io.BytesIO()
+        if log(numP, 2) % 2 == 0:
+            can = canvas.Canvas(packet, pagesize=A4)
+            cut_line_width, cut_line_height = A4  # Full width of the page # Full height of the page
+        else:
+            can = canvas.Canvas(packet, pagesize=(A4[1], A4[0]))
+            cut_line_height, cut_line_width = A4  # Full width of the page # Full height of the page
+        cut_line_x = 0  # X position for the horizontal cut line
+        cut_line_y = 0  # Y position for the vertical cut line
+
+        can.setStrokeColorRGB(0, 0, 0)  # Black color for the cut lines
+        can.setDash(3, 3)  # Set dash pattern (3 units on, 3 units off)
+        can.setLineWidth(2)
+        if log(numP, 2) % 2 == 0:
+            for j in range(1, int(sqrt(numP))):
+                # Add horizontal cut line
+                can.line(cut_line_x, j * cut_line_height / int(sqrt(numP)), cut_line_x + cut_line_width,
+                         j * cut_line_height / int(sqrt(numP)))
+                if j % 2 == 0:
+                    # Add vertical cut line
+                    can.line(j * cut_line_width / int(sqrt(numP)), cut_line_y, j * cut_line_width / int(sqrt(numP)),
+                             cut_line_y + cut_line_height)
+        else:
+            y = int(sqrt(numP / 2))
+            x = y * 2
+            for j in range(1, y):
+                # Add horizontal cut line
+                can.line(cut_line_x, j * cut_line_height / y, cut_line_x + cut_line_width,
+                         j * cut_line_height / y)
+            for j in range(1, x):
+                if j % 2 == 0:
+                    can.line(j * cut_line_width / x, cut_line_y, j * cut_line_width / x,
+                             cut_line_y + cut_line_height)
+        can.save()
+        packet.seek(0)
+        new_pdf = PdfFileReader(packet)
+        pdf.pages[i].merge_page(new_pdf.pages[0])
+        output_pdf.addPage(pdf.pages[i])  # Add the modified page to the output
+    with open(file, "wb") as output:
+        output_pdf.write(output)
 
 
 # ~~~~~~~~~~~~~~~~~
 #     main ♪♫♪
 # ~~~~~~~~~~~~~~~~~
 
-if __name__ == '__main__':
 
+def main(inputs):
     start = True
     while start:
-        inputs = UI()  # calling the UI
-        start=False
+        start = False
         th = Thread(target=making_the_pdf, args=[inputs, 1])
         th.start()
 
@@ -247,3 +376,7 @@ if __name__ == '__main__':
                 start = True
                 break
         window_end.close()
+
+
+if __name__ == '__main__':
+    main(UI())
---
+++ tests/pocket_book_system_test.py
@@ -0,0 +1,62 @@
+import io
+import os
+import json
+import pytest
+from tempfile import mkdtemp
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from src.pocket_book import making_the_pdf, PageSize
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfgen import canvas
+
+test_dir = mkdtemp()
+print(f"test_dir={test_dir}")
+
+def create_test_pdf(pages_num):
+    existing_pdf = PdfFileWriter()
+    for i in range(pages_num):
+        packet = io.BytesIO()
+        can = canvas.Canvas(packet, pagesize=A4)
+        can.setFontSize(80)
+        can.drawString(270, 200, str(i))
+        can.save()
+        packet.seek(0)
+        # create a new PDF with Reportlab
+        new_pdf = PdfFileReader(packet)
+        # read your existing PDF
+        existing_pdf.addBlankPage(A4[0], A4[1])
+        # add the "watermark" (which is the new pdf) on the existing page
+        existing_pdf.pages[i].merge_page(new_pdf.pages[0])
+        # finally, write "output" to a real file
+
+    test_file_path = os.path.join(test_dir, f"{pages_num}pages.pdf")
+    with open(test_file_path, "wb") as f:
+        existing_pdf.write(f)
+
+    return test_file_path
+
+@pytest.mark.parametrize("pages_num, notebook_len, page_size, bind_method, combine_method, language", [
+    (20, 32, PageSize.A7.value, '', 'v',0), # זוגי פחות מגודל חוברת
+    (25, 16, PageSize.A7.value, 's', 'v', 0),  # אי זוגי פחות מגודל חוברת
+    (33, 8, PageSize.A7.value, 's', 'v', 0),  # אי זוגי גדול מגודל חוברת
+    (40, 4, PageSize.A7.value, 's', 'v', 0),  # זוגי גדול מגודל חוברת
+    (64, 32, PageSize.A6.value, 's', 'v', 0),  # זוגי שתי חוברות בדיוק
+    (101, 32, PageSize.A6.value, 's', 'v', 0),  # אי זוגי יותר משתי חוברות
+    (102, 32, PageSize.A7.value, 's', 'v', 1),  # באנגלית
+])
+def test_pocketing_book(pages_num, notebook_len, page_size, bind_method, combine_method, language):
+    test_pdf_path = create_test_pdf(pages_num)
+
+    making_the_pdf([test_pdf_path, notebook_len, page_size, bind_method, combine_method, language])
+
+    with open(os.path.join("tests", "test_outputs.json"), 'r') as f:
+        expected_outputs = json.load(f)
+
+    pocketed_pdf_path = test_pdf_path[:-4] + " ready to print.pdf"
+    pocketed_pdf = PdfFileReader(open(pocketed_pdf_path, "rb"))
+    pdf_text=""
+    for i in range(len(pocketed_pdf.pages)):
+        page = pocketed_pdf.pages[i]
+        pdf_text += page.extract_text()
+
+    assert expected_outputs[str(pages_num)] == pdf_text
